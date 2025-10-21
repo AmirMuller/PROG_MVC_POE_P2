860d6c8f4b504416d06cbecdc3f7d17c07861503
Amir-rb <amir.muller@gmail.com>
2025-10-21 02:42:06 +0200

    Added Submit Claims

---
+++ PROG_MVC_POE_P2/Controllers/ClaimsController/ClaimsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROG_MVC_POE_P2.Models;
+
+namespace PROG_MVC_POE_P2.Controllers.Request
+{
+    public class ClaimsController : Controller
+    {
+        private static List<LecturerModel> _lecturer = new List<LecturerModel>
+        {
+            new LecturerModel { LecturerId = 1, Fname = "John", Lname = "Doe", NumHours = 48, Rate = 200 },
+            new LecturerModel { LecturerId = 2, Fname = "Peter", Lname = "Doe", NumHours = 50, Rate = 180 }
+        };
+
+        private static List<Claim> _claims = new List<Claim>();
+
+        public IActionResult Index()
+        {
+            ViewBag.Lecturers = _lecturer;
+            return View(_claims);
+        }
+
+
+        public IActionResult Create()
+        {
+            ViewBag.Lecturers = _lecturer;
+            return View("SubmitClaim");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SubmitClaim(Claim claim)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Lecturers = _lecturer;
+                return View("SubmitClaim", claim);
+            }
+
+            claim.ClaimId = _claims.Any() ? _claims.Max(r => r.ClaimId) + 1 : 1;
+            claim.Status = "Pending";
+            _claims.Add(claim);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
---
+++ PROG_MVC_POE_P2/Models/Claim.cs
@@ -0,0 +1,10 @@
+﻿namespace PROG_MVC_POE_P2.Models
+{
+    public class Claim
+    {
+        public int ClaimId { get; set; }
+        public int LecturerId { get; set; }
+        public DateTime ClaimTime { get; set; }
+        public string Status { get; set; }
+    }
+}
